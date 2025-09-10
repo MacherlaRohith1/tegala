420f5b1547e9bf6ebdeb2c3858d2fd6f17e0c72a
mache <mache@Rohith>
2025-09-11 00:28:22 +0530

    first commit

---
+++ javacore/src/com/tnsif/first/demo.java
@@ -0,0 +1,15 @@
+package com.tnsif.first;
+
+public class demo {
+
+	public demo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first class")
+
+	}
+
+}
