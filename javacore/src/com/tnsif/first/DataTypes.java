d5a809b2e74821b4b73891a3cbbde89edf584dad
mache <mache@Rohith>
2025-09-11 23:02:04 +0530

    datatypes

---
+++ javacore/src/com/tnsif/first/DataTypes.java
@@ -0,0 +1,28 @@
+package com.tnsif.first;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte byteVar = 100;
+        short shortVar = 30000;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+
+        float floatVar = 10.5f;
+        double doubleVar = 20.123456;
+
+        char charVar = 'A';
+        boolean boolVar = true;
+
+        String strVar = "Hello, Java!";
+
+        System.out.println("Byte value: " + byteVar);
+        System.out.println("Short value: " + shortVar);
+        System.out.println("Int value: " + intVar);
+        System.out.println("Long value: " + longVar);
+        System.out.println("Float value: " + floatVar);
+        System.out.println("Double value: " + doubleVar);
+        System.out.println("Char value: " + charVar);
+        System.out.println("Boolean value: " + boolVar);
+        System.out.println("String value: " + strVar);
+    }
+}
