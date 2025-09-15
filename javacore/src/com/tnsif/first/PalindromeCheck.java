ff63cb74792cbc4bce9615da73865c08a4b055bd
mache <mache@192.168.1.64>
2025-09-15 23:38:55 +0530

    Palindrome

---
+++ javacore/src/com/tnsif/first/PalindromeCheck.java
@@ -0,0 +1,23 @@
+package com.tnsif.first;
+
+import java.util.Scanner;
+
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num, reverse = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        if (reverse == original)
+            System.out.println(original + " is a Palindrome");
+        else
+            System.out.println(original + " is Not a Palindrome");
+    }
+}
