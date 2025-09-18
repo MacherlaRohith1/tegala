ea0bf2deda7e0af6096826a105d5ce8f45d5bec3
mache <mache@192.168.1.34>
2025-09-18 23:10:49 +0530

    interface

---
+++ javacore/src/com/tnsif/interfacee/Main.java
@@ -0,0 +1,12 @@
+package com.tnsif.interfacee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Jio j =new Jio();
+		Samsung s=new Samsung();
+		j.show();
+		s.show();
+	}
+
+}
---
+++ javacore/src/com/tnsif/interfacee/phone.java
@@ -0,0 +1,24 @@
+package com.tnsif.interfacee;
+public interface phone {
+	String  Call="Calling..";
+	String  Sms="Sending SMS..";
+	void show();
+}
+class Jio implements phone{
+	public void show()
+	{
+		System.out.println("This is Jio phone");
+		System.out.println(Call);
+		System.out.println(Sms);
+
+	}
+}
+class Samsung implements phone{
+	public void show()
+	{
+		System.out.println("This is Samsung phone");
+		System.out.println(Call);
+		System.out.println(Sms);
+
+	}
+}
