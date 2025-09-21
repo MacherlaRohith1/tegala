a820bdb44589e60193f7ae1f21cb3afa083dfcea
mache <mache@192.168.1.57>
2025-09-21 10:09:31 +0530

    String Operations

---
+++ javacore/src/com/tnsif/nintheen/StringOpp.java
@@ -0,0 +1,16 @@
+package com.tnsif.nintheen;
+
+public class StringOpp {
+public static void main(String[] args) {
+        
+        String str = "Hello World";
+
+        System.out.println("Original String: " + str);
+        System.out.println("Length: " + str.length());
+        System.out.println("Uppercase: " + str.toUpperCase());
+        System.out.println("Lowercase: " + str.toLowerCase());
+        System.out.println("Substring (0 to 5): " + str.substring(0, 5));
+    }
+}
+
+
