ea1c944f91f0fe91f74ae651d89ecea0193f3998
mache <mache@192.168.1.57>
2025-09-21 10:22:08 +0530

    interface exyends interface

---
+++ javacore/src/com/tnsif/interexeinter/Main.java
@@ -0,0 +1,13 @@
+package com.tnsif.interexeinter;
+
+public class Main {
+	public static void main(String[] args) {
+        Dog d=new Dog();
+        d.eat();
+        d.sleep();
+        d.shout();
+	}
+
+}
+
+
